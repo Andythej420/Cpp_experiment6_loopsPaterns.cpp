34a693e47fea545842e9d0f4c07429c8e811b4f0
Andythej420 <dearanjaneya@gmail.com>
2025-09-24 11:29:02 +0530

    Create password.cpp

---
+++ password.cpp
@@ -0,0 +1,32 @@
+// Anjaneya Kalchuri
+// PRN - 24070123015
+// ENTC A1
+// EXPERIMENT 06
+
+#include <iostream>
+using namespace std;
+int main() {
+    int password =123;
+    int input;
+    int triesLeft =3;
+
+    cout << "Secure Area Login\n";
+    while (triesLeft) {
+        cout << "\nPassword: (Remaining attempts: " << triesLeft << "): ";
+        cin >> input;
+
+        if (input == password) {
+            cout << "Login successful. Access granted.\n";
+            break;
+        } else {
+            cout << "Wrong password. Try again.\n";
+            triesLeft--;
+        }
+    }
+
+    if (triesLeft == 0) {
+        cout << "\nMaximum attempts exceeded. Access blocked.\n";
+    }
+
+    return 0;
+}
