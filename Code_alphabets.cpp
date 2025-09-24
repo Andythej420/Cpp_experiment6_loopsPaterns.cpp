69719a8df7869f855e13cc5114128cda89589f36
Andythej420 <dearanjaneya@gmail.com>
2025-09-24 11:21:17 +0530

    Create Code_alphabets.cpp

---
+++ Code_alphabets.cpp
@@ -0,0 +1,20 @@
+// Anjaneya Kalchuri
+// PRN - 24070123015
+// ENTC A1
+// EXPERIMENT 06
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char n = 7;
+    char ch='A';
+    for(char i = 1; i <= n; i++) {
+        for(char j = 1; j <= i; j++) {
+            cout << ch << " ";
+            ch++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
