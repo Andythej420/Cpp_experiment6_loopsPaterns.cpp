4302adaca21725b7c85d89e1e711d634cac78d75
Andythej420 <dearanjaneya@gmail.com>
2025-09-24 11:29:44 +0530

    Create right angle left align numbers.cpp

---
+++ right angle left align numbers.cpp
@@ -0,0 +1,19 @@
+// Anjaneya Kalchuri
+// PRN - 24070123015
+// ENTC A1
+// EXPERIMENT 06
+
+#include <iostream>
+using namespace std;
+int main() {
+    int a=1;
+    int row =4;
+    for (int i = 1; i <= row; i++) {
+        for (int j = 1; j <= i; j++) {
+            if (a<= 10)
+                cout << a++ << " ";
+        }
+        cout << endl;
+    }
+   return 0;
+}
