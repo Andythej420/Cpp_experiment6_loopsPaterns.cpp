937a4f2e671adff9d585aeb435c3353ee5b32ebc
Andythej420 <dearanjaneya@gmail.com>
2025-09-24 11:26:37 +0530

    Create for_loop_SIT.cpp

---
+++ for_loop_SIT.cpp
@@ -0,0 +1,15 @@
+// Anjaneya Kalchuri
+// PRN - 24070123015
+// ENTC A1
+// EXPERIMENT 06
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 5; i++) {
+        cout<< "SIT" <<endl;
+    }
+    return 0;
+}
